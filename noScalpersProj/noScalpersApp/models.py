015f911f3bf01d63684380f5cd1604c99ffee2ec
James <tytechortz@gmail.com>
2018-11-19 12:11:17 -0700

    adds admin functionality

--- noScalpersProj/noScalpersApp/models.py
+++ noScalpersProj/noScalpersApp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    commentBody = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='post')
+
+def __str__(self):
+    return self.title
